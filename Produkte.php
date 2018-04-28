88adda5f944468f392e61a50134b693efb62e737
Jan Blömer <Jabl2014jb@gmail.com>
2018-04-28 08:47:14 +0200

    Erstellung

---
+++ Produkte.php
@@ -0,0 +1,55 @@
+<!DOCTYPE html>
+
+<html>
+
+<head>
+    <title>Apendix Webdesign | Willkommen!</title>
+	<link rel="shortcut icon" href="/img/titile.png" />
+	<link rel="stylesheet" href="/css/style.css"/>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width">
+	<meta name="description" content="">
+	<meta name="keywords" content="website billig, website hosting, vechta">
+  	<meta name="author" content="Jan Blömer,Dirk">
+</head>
+<body>
+	 <header>
+      <div class="container">
+        <div id="branding">
+          <h1><span class="highlight">Apendix</span> Web Design</h1>
+        </div>
+        <nav>
+          <ul>
+            <li><a href="index.php">Home</a></li>
+            <li  class="current"><a href="Produkte.php">Angebote</a></li>
+	    <li><a href="about.php">&Uuml;ber uns</a></li>
+          </ul>
+        </nav>
+      </div>
+    </header>
+    <article id="main-col">
+          <ul id="services">
+            <li>
+              <h3>Website Design</h3>
+              <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus mi augue, viverra sit amet ultricies at, vulputate id lorem. Nulla facilisi.</p>
+						  <p>Pricing: $1,000 - $3,000</p>
+            </li>
+            <li>
+              <h3>Website Maintenance</h3>
+              <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus mi augue, viverra sit amet ultricies at, vulputate id lorem. Nulla facilisi.</p>
+						  <p>Pricing: $250 per month</p>
+            </li>
+            <li>
+              <h3>Website Hosting</h3>
+              <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus mi augue, viverra sit amet ultricies at, vulputate id lorem. Nulla facilisi.</p>
+						  <p>Pricing: $25 per month</p>
+            </li>
+          </ul>
+        </article>
+	 <footer>
+      <p> Name, Copyright &copy; 2018</p>
+    </footer>
+</body>
+
+</html>
+	
