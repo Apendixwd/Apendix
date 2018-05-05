5a4681f4229fb0e4c6b5885eecbd293a69ec6321
Jan Blömer <Jabl2014jb@gmail.com>
2018-05-05 09:27:00 +0200

    Erstellung

---
+++ Kontakt.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+
+<html>
+
+<head>
+    <title>Apendix Webdesign | Willkommen!</title>
+	<link rel="shortcut icon" href="/img/titile.png" />
+	<link rel="stylesheet" href="/css/style.css"/>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width">
+	<meta name="description" content="">
+	<meta name="keywords" content="website billig, website hosting, vechta">
+  	<meta name="author" content="Jan Blömer,Dirk">
+</head>
+
+<body>
+	 <header>
+      <div class="container">
+        <div id="branding">
+          <h1><span class="highlight">Apendix</span> Web Design</h1>
+        </div>
+        <nav>
+          <ul>
+            <li><a href="index.php">Home</a></li>
+            <li><a href="Produkte.php">Angebote</a></li>
+			<li><a href="about.php">Über uns</a></li>
+		    <li class="current"><a href="Kontakt.php">Kontakt</a></li> 								 
+          </ul>
+        </nav>
+      </div>
+    </header>
+
+
+</body>
+
+</html>
